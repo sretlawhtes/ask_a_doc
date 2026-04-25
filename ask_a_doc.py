eef9cb45a59dafbf0dae09b2f003d5b1dc1ccc63
sretlawhtes <seth.walters49@gmail.com>
2026-04-25 16:26:10 -0500

    Update ask_a_doc.py

--- ask_a_doc.py
+++ ask_a_doc.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
@@ -13,9 +13,9 @@ def generate_response (uploaded_file, openai_api_key, query_text):
         text_splitter=CharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
         texts=text_splitter.create_documents(documents)
         #Select embeddings
-        embeddings=OpenAIEmbeddings(openai_api_key=open_ai_key)
+        embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key)
         #Create a vectorstore from documents
-        db=Chroma.from_documents(text,embeddings)
+        db=Chroma.from_documents(texts,embeddings)
         #Create retriever interface
         retriever=db.as_retriever()
         #Create QA chain
